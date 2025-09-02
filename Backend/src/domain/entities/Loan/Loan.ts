1ea175caba89abbdb6d094ba2dd4ccaa74f4ceb3
david123456858 <juandavidperaltafuentes@gmail.com>
2025-09-02 11:13:48 -0500

    now yes domain

---
+++ Backend/src/domain/entities/Loan/Loan.ts
@@ -0,0 +1,54 @@
+export class Loan {
+  private id: string
+  private idUser: string
+  private idStationOrigin: string
+  private idStationDestination: string
+  private dateStart: Date
+  constructor (id: string, idUser: string, idStationOrigin: string, idStationDestination: string, dateStart: Date) {
+    this.id = id
+    this.idUser = idUser
+    this.idStationOrigin = idStationOrigin
+    this.idStationDestination = idStationDestination
+    this.dateStart = dateStart
+  }
+
+  getId (): string {
+    return this.id
+  }
+
+  setId (id: string): void {
+    this.id = id
+  }
+
+  getIdUser (): string {
+    return this.idUser
+  }
+
+  setIdUser (idUser: string): void {
+    this.idUser = idUser
+  }
+
+  getIdStationOrigin (): string {
+    return this.idStationOrigin
+  }
+
+  setIdStationOrigin (idStationOrigin: string): void {
+    this.idStationOrigin = idStationOrigin
+  }
+
+  setIdStationDestination (idStationDestination: string): void {
+    this.idStationDestination = idStationDestination
+  }
+
+  setDateStart (dateStart: Date): void {
+    this.dateStart = dateStart
+  }
+
+  getDateStart (): Date {
+    return this.dateStart
+  }
+
+  getIdStationDestination (): string {
+    return this.idStationDestination
+  }
+}
